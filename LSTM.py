06f98f67f1d1515ba66cb2641ad61f3d7030f880
CHI30 <chj30@outlook.com>
2025-06-28 18:24:06 +0800

    Add initial project files including LSTM model, requirements, and data handling scripts

---
+++ LSTM.py
@@ -0,0 +1,71 @@
+# Make sure that you have all these libaries available to run the code successfully
+from pandas_datareader import data
+import matplotlib.pyplot as plt
+import pandas as pd
+import datetime as dt
+import urllib.request, json
+import os
+import numpy as np
+import tensorflow as tf # This code has been tested with TensorFlow 1.6
+from sklearn.preprocessing import MinMaxScaler
+
+
+# step1: load data
+df = pd.read_csv(os.path.join('data/Stocks','hpq.us.txt'),delimiter=',',usecols=['Date','Open','High','Low','Close'])
+print('Loaded data from the Kaggle repository')
+
+# make sure data in data order
+df = df.sort_values('Date')
+
+# Double check the result
+df.head()
+print(df.head())
+
+#Data visualization
+
+plt.figure(figsize = (18,9))
+plt.plot(range(df.shape[0]),(df['Low']+df['High'])/2.0)
+plt.xticks(range(0,df.shape[0],500),df['Date'].loc[::500],rotation=45)
+plt.xlabel('Date',fontsize=18)
+plt.ylabel('Mid Price',fontsize=18)
+plt.show()
+
+# split data
+# First calculate the mid prices from the highest and lowest
+high_prices = df.loc[:,'High'].as_matrix()
+low_prices = df.loc[:,'Low'].as_matrix()
+mid_prices = (high_prices+low_prices)/2.0
+
+train_data = mid_prices[:11000]
+test_data = mid_prices[11000:]
+
+# Scale the data to be between 0 and 1
+
+scaler = MinMaxScaler()
+train_data = train_data.reshape(-1,1)
+test_data = test_data.reshape(-1,1)
+# Train the Scaler with training data and smooth data
+smoothing_window_size = 2500
+for di in range(0,10000,smoothing_window_size):
+    scaler.fit(train_data[di:di+smoothing_window_size,:])
+    train_data[di:di+smoothing_window_size,:] = scaler.transform(train_data[di:di+smoothing_window_size,:])
+
+# You normalize the last bit of remaining data
+scaler.fit(train_data[di+smoothing_window_size:,:])
+train_data[di+smoothing_window_size:,:] = scaler.transform(train_data[di+smoothing_window_size:,:])
+
+# Reshape both train and test data
+train_data = train_data.reshape(-1)
+
+# Normalize test data
+test_data = scaler.transform(test_data).reshape(-1)
+# Now perform exponential moving average smoothing
+# So the data will have a smoother curve than the original ragged data
+EMA = 0.0
+gamma = 0.1
+for ti in range(11000):
+  EMA = gamma*train_data[ti] + (1-gamma)*EMA
+  train_data[ti] = EMA
+
+# Used for visualization and test purposes
+all_mid_data = np.concatenate([train_data,test_data],axis=0)
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # PV_solar_prediction
 PV solar prediction by machine learning
+E:\ANACONDA;E:\ANACONDA\Scripts;E:\ANACONDA\Library\mingw-w64\bin;E:\ANACONDA\Library\usr\bin;E:\ANACONDA\Library\bin;
