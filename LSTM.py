a6df3b0442acb5d59a99f139e98793cdeeb0e763
CHI30 <chj30@outlook.com>
2025-06-29 15:51:39 +0800

    Refactor data loading and visualization; replace deprecated methods and enhance prediction calculations with standard and exponential moving averages.

--- LSTM.py
+++ LSTM.py
@@ -19,7 +19,7 @@ df = df.sort_values('Date')
 
 # Double check the result
 df.head()
-print(df.head())
+
 
 #Data visualization
 
@@ -32,8 +32,8 @@ plt.show()
 
 # split data
 # First calculate the mid prices from the highest and lowest
-high_prices = df.loc[:,'High'].as_matrix()
-low_prices = df.loc[:,'Low'].as_matrix()
+high_prices = df.loc[:,'High'].to_numpy()
+low_prices = df.loc[:,'Low'].to_numpy()
 mid_prices = (high_prices+low_prices)/2.0
 
 train_data = mid_prices[:11000]
@@ -64,8 +64,68 @@ test_data = scaler.transform(test_data).reshape(-1)
 EMA = 0.0
 gamma = 0.1
 for ti in range(11000):
-  EMA = gamma*train_data[ti] + (1-gamma)*EMA
-  train_data[ti] = EMA
+    EMA = gamma*train_data[ti] + (1-gamma)*EMA
+    train_data[ti] = EMA
 
 # Used for visualization and test purposes
-all_mid_data = np.concatenate([train_data,test_data],axis=0)
+all_mid_data = np.concatenate([train_data, test_data], axis=0)
+
+# Standard average
+window_size = 100
+N = train_data.size
+std_avg_predictions = []
+std_avg_x = []
+mse_errors = []
+
+for pred_idx in range(window_size,N):
+
+    if pred_idx >= N:
+        date = dt.datetime.strptime(k, '%Y-%m-%d').date() + dt.timedelta(days=1)
+    else:
+        date = df.loc[pred_idx,'Date']
+
+    std_avg_predictions.append(np.mean(train_data[pred_idx-window_size:pred_idx]))
+    mse_errors.append((std_avg_predictions[-1]-train_data[pred_idx])**2)
+    std_avg_x.append(date)
+
+print('MSE error for standard averaging: %.5f' % (0.5 * np.mean(mse_errors)))
+# show avag print
+plt.figure(figsize = (18,9))
+plt.plot(range(df.shape[0]),all_mid_data,color='b',label='True')
+plt.plot(range(window_size,N),std_avg_predictions,color='orange',label='Prediction')
+#plt.xticks(range(0,df.shape[0],50),df['Date'].loc[::50],rotation=45)
+plt.xlabel('Date')
+plt.ylabel('Mid Price')
+plt.legend(fontsize=18)
+plt.show()
+# Exponential moving average
+window_size = 100
+N = train_data.size
+
+run_avg_predictions = []
+run_avg_x = []
+
+mse_errors = []
+
+running_mean = 0.0
+run_avg_predictions.append(running_mean)
+
+decay = 0.5
+
+for pred_idx in range(1,N):
+
+    running_mean = running_mean*decay + (1.0-decay)*train_data[pred_idx-1]
+    run_avg_predictions.append(running_mean)
+    mse_errors.append((run_avg_predictions[-1]-train_data[pred_idx])**2)
+    run_avg_x.append(date)
+
+print('MSE error for EMA averaging: %.5f' % (0.5 * np.mean(mse_errors)))
+# show
+plt.figure(figsize = (18,9))
+plt.plot(range(df.shape[0]),all_mid_data,color='b',label='True')
+plt.plot(range(0,N),run_avg_predictions,color='orange', label='Prediction')
+#plt.xticks(range(0,df.shape[0],50),df['Date'].loc[::50],rotation=45)
+plt.xlabel('Date')
+plt.ylabel('Mid Price')
+plt.legend(fontsize=18)
+plt.show()
